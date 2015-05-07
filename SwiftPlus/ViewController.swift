feaec93381b12f3ff0ae2be3816c66dbdcd72d5e
Andrei Papancea <andrei@max2.com>
2015-05-07 16:39:02 -0400

    Initial Commit

---
+++ SwiftPlus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftPlus
+//
+//  Created by Andrei Papancea on 5/7/15.
+//  Copyright (c) 2015 Andrei Papancea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
